0ce34dad42cd72b55b16f1d08815e9dd41128cf7
nichuanlei <nichuanlei@qq.com>
2016-06-20 13:07:12 +0800

    初始化一个项目

---
+++ weiwei/weiwei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weiwei
+//
+//  Created by 倪传磊 on 16/6/20.
+//  Copyright © 2016年 nichuanlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weiwei/weiwei/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weiwei
+//
+//  Created by 倪传磊 on 16/6/20.
+//  Copyright © 2016年 nichuanlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
